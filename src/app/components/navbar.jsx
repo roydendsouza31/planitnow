bee4c378e49fbda084827facb0c4a4a1cadf2616
AlishkaFernandes <alishkafernandes19@hotmail.com>
2024-08-25 17:38:17 +0530

    contact form

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -4,7 +4,7 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 const navigation = [
   { name: 'Home', href: '/', current: true },
   { name: 'Services', href: '#', current: false },
-  { name: 'Contact Us', href: '#', current: false },
+  { name: 'Contact Us', href: '/contact', current: false },
   { name: 'About', href: '/about', current: false },
   
 ]
