ba6d95d6a25647212ba9a72afdb821207314e917
Royden Dsouza <royden@192.168.0.230>
2024-08-25 14:54:30 +0530

    resolve merge conflict

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,8 @@
 import Navbar from "./components/Navbar";
 import Filter from "./components/Filter";
 import Seller from "./components/Seller";
-<<<<<<< HEAD
-import Footer from "./components/footer";
 import PopularPhotographers from "./components/popularphotographer";
-=======
 import Footer from "./components/Footer";
->>>>>>> 89b535f1bc04cf6b71cf3e95bc2d79d0996f7bb3
 
 export default function Home() {
   return (
